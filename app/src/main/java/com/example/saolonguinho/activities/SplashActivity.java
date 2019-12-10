86aa8506db2b4813b4dd860437e9de4fcbed6ad0
eraldoakosoft <eraldo450518@outlook.com>
2019-12-09 23:08:45 -0300

    Mudei a cor do App

--- app/src/main/java/com/example/saolonguinho/activities/SplashActivity.java
+++ app/src/main/java/com/example/saolonguinho/activities/SplashActivity.java
@@ -23,6 +23,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
